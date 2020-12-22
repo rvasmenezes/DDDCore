d859067d7a349f3b607026c39103eef042b8c967
rvasmenezes <rvasmenezes@gmail.com>
2020-12-22 15:50:01 -0300

    Projeto de teste DDD Core

---
+++ Infra/Config/ContextBase.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Config
+{
+    public class ContextBase : DbContext
+    {
+
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+
+        }
+     
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(StringConnectionConfig());
+            }
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        private string StringConnectionConfig()
+        {
+            string strConf = "server=DESKTOP-S8VP4K4\\SQLEXPRESS;User Id=sa;password=123456;Persist Security Info=True;database=DDDTesteCore";
+            //string strConf = "Server=(localdb)\\mssqllocaldb;Database=DDDTesteCore;Trusted_Connection=True;";
+            return strConf;
+        }
+    }
+}
